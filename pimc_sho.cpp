910e5572f854c8700fd3eee44b8ca99a45970e63
xfzhang <xfzhang.phys@outlook.com>
2020-04-28 22:20:55 +0800

    Can read parameters from command line

--- pimc_sho.cpp
+++ pimc_sho.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <string>
 #include "path.h"
 #include "estimator.h"
 
 
 int main(int argc, char** argv) {
-    int nbeads = 16;
+    int nbeads = 32;
     int nsteps = 1e6;
     int nthermal = 2e5;
     int nbins = 1e4;
     double temp = 0.2;
     double beta = 1.0 / temp;
     double max_disp = 0.5;
+
+    // read parameters from command line
+    if (argc > 1) {
+        cout << "You should type arguments like: pimc.x [temp] [nbeads] [nsteps] [nthermal]" << endl;
+        temp = stof(string(argv[1])); beta = 1.0 / temp;
+        nbeads = stoi(string(argv[2]));
+        nsteps = stoi(string(argv[3]));  nbins = nsteps / 100; 
+        nthermal = stoi(string(argv[4]));
+    }
 
     // random number generator
     random_device r;
