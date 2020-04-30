4d266fdfd6f8d9da2871612bb658dce9487bf4e3
xfzhang <xfzhang.phys@outlook.com>
2020-04-30 23:04:42 +0800

    Back to use at rather than []

--- pimc_sho.cpp
+++ pimc_sho.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <string>
+//#include <chrono>
 #include "path.h"
 #include "estimator.h"
 #include "input.h"
 
+//using namespace std::chrono;
 
 int main(int argc, char** argv) {
 
-    Input input_params(16, 1e6, 2e5, 1e4, 0.5, 0.5, "PCV");
+    // timer
+    //auto start = steady_clock::now();
+
+    Input input_params(32, 1e6, 2e5, 1e4, 0.5, 0.5, "T");
     // read parameters from command line
     if (argc > 1) {
         input_params.get_input_params(argc, argv);
@@ -50,4 +55,11 @@ int main(int argc, char** argv) {
     }
 
     estimator.output();
+
+    // timer
+    //auto end = steady_clock::now();
+    //auto elapsed_time = duration_cast<seconds> (end - start);
+    //cout << "Elapsed time: " << elapsed_time.count() << "s." << endl;
+    int tmp;
+    cin >> tmp;
 }
